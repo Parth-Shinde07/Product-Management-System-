ae2c9780f18ba096062e2b903cf5cb32af39d3f8
Parth <parth@Parths-MacBook-Air.local>
2026-07-07 21:58:56 +0530

    updated db connection

--- src/main/java/Product_Management/example/API/Model/Product.java
+++ src/main/java/Product_Management/example/API/Model/Product.java
@@ -1,13 +1,11 @@
 package Product_Management.example.API.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data // generates getters,setters & tostring via lombook
+@Table(name="products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
